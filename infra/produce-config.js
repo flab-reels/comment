291659421c4652f53eedc0deb618ce055e7ad808
HospitalBus <simony1124@gmail.com>
2022-12-14 21:09:06 +0900

    generates required files for deployment

--- infra/produce-config.js
+++ infra/produce-config.js
@@ -3,29 +3,36 @@
 const aws = require('aws-sdk');
 const fs = require('fs');
 
+//const argv = require('yargs')
+//    .usage('Create ECS/CodeDeploy config files with values from CloudFormation stacks\nUsage: $0')
+//    .demandOption(['s', 'd'])
+//    .alias('s', 'infra-stack-name')
+//    .alias('d', 'service-stack-name')
+////    .alias('g', 'stage-name')
+////    .alias('h', 'hook-stack-name')
+//    .argv;
 const argv = require('yargs')
     .usage('Create ECS/CodeDeploy config files with values from CloudFormation stacks\nUsage: $0')
-    .demandOption(['s', 'd', 'g', 'h'])
+    .demandOption(['s'])
     .alias('s', 'infra-stack-name')
-    .alias('d', 'service-stack-name')
-    .alias('g', 'stage-name')
-    .alias('h', 'hook-stack-name')
     .argv;
 
+// load json file
 const taskDefConfig = require('./task-definition.json');
 const appSpec = require('./appspec.json');
 
-const stage = argv.stageName;
+//const stage = argv.stageName;
 const infraStack = argv.infraStackName;
-const serviceStack = argv.serviceStackName;
-const hookStack = argv.hookStackName;
+//const serviceStack = argv.serviceStackName;
+//const hookStack = argv.hookStackName;
 
+aws.config.update({region : 'ap-northeast-2'})
 const cfn = new aws.CloudFormation();
 
 async function produceConfigs() {
     let infraData = await cfn.describeStackResources({ StackName: infraStack }).promise();
-    let serviceData = await cfn.describeStackResources({ StackName: serviceStack }).promise();
-    let hookData = await cfn.describeStackResources({ StackName: hookStack }).promise();
+//    let serviceData = await cfn.describeStackResources({ StackName: serviceStack }).promise();
+//    let hookData = await cfn.describeStackResources({ StackName: hookStack }).promise();
 
     // Make a whole bunch of assumptions about the contents of the CFN stacks
     let privateSubnets = [];
@@ -36,40 +43,42 @@ async function produceConfigs() {
 
     for (const resource of infraData.StackResources) {
         if (resource.ResourceType == 'AWS::EC2::Subnet' &&
-                    resource.LogicalResourceId.startsWith('VPCPrivateSubnet')) {
+                    resource.LogicalResourceId.startsWith('samplecommentvpcPrivate')) {
             privateSubnets.push(resource.PhysicalResourceId);
+            console.log(resource.PhysicalResourceId)
         } else if (resource.ResourceType == 'AWS::EC2::SecurityGroup' &&
-                    resource.LogicalResourceId.startsWith('ServiceSecurityGroup')) {
+                    resource.LogicalResourceId.startsWith('samplecommentservicesg')) {
             serviceSecurityGroups.push(resource.PhysicalResourceId);
+            console.log(resource.PhysicalResourceId)
         }
-    }
 
-    for (const resource of serviceData.StackResources) {
         if (resource.ResourceType == 'AWS::IAM::Role' &&
-                    resource.LogicalResourceId.startsWith('TaskDefExecutionRole')) {
+            resource.LogicalResourceId.startsWith('SampleCommentTaskDefExecutionRole')) {
             executionRole = resource.PhysicalResourceId;
+            console.log(resource.PhysicalResourceId);
         } else if (resource.ResourceType == 'AWS::IAM::Role' &&
-                    resource.LogicalResourceId.startsWith('TaskDefTaskRole')) {
+            resource.LogicalResourceId.startsWith('SampleCommentTaskDefTaskRole')) {
             taskRole = resource.PhysicalResourceId;
+            console.log(resource.PhysicalResourceId);
         }
     }
 
-    for (const resource of hookData.StackResources) {
-        if (resource.LogicalResourceId == 'PreTrafficHook') {
-            preTrafficHook = resource.PhysicalResourceId;
-        }
-    }
+//    for (const resource of hookData.StackResources) {
+//        if (resource.LogicalResourceId == 'PreTrafficHook') {
+//            preTrafficHook = resource.PhysicalResourceId;
+//        }
+//    }
 
     // Write out task def config
     taskDefConfig.taskRoleArn = taskRole;
     taskDefConfig.executionRoleArn = executionRole;
-    fs.writeFileSync(`./build/task-definition-${stage}.json`, JSON.stringify(taskDefConfig, null, 2) , 'utf-8');
+    fs.writeFileSync(`./build/task-definition-sample.json`, JSON.stringify(taskDefConfig, null, 2) , 'utf-8');
 
     // Write out appspec
     appSpec.Resources[0].TargetService.Properties.NetworkConfiguration.awsvpcConfiguration.subnets = privateSubnets;
     appSpec.Resources[0].TargetService.Properties.NetworkConfiguration.awsvpcConfiguration.securityGroups = serviceSecurityGroups;
     appSpec.Hooks[0].AfterAllowTestTraffic = preTrafficHook;
-    fs.writeFileSync(`./build/appspec-${stage}.json`, JSON.stringify(appSpec, null, 2) , 'utf-8');
+    fs.writeFileSync(`./build/appspec-sample.json`, JSON.stringify(appSpec, null, 2) , 'utf-8');
 }
 
 produceConfigs().catch(err => {
